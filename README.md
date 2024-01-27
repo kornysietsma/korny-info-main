f4ab16d8bc0bc47f4b9166df7736c2c997f20542
Korny Sietsma <korny.sietsma@liberis.com>
2024-01-27 18:56:06 +0000

    reworked with very basic simple.css styling

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # the main korny.info website
 
-This is still an ancient site, from 2010-ish, with tiny hacks since then.
-
-It needs to be rebuilt, but I'm republishing it via github pages just so
-I can move it from my old ISP.
-
-Please ignore the ugliness!
+My very minimal site - now using [simple.css](https://simplecss.org) for very basic styling.
--- javascript/korny_main.js
+++ javascript/korny_main.js
@@ -1,24 +1,9 @@
-(function() {
-  var KornyMain, demunge;
-  demunge = function(str) {
-    var l;
-    l = str.length;
-    return str[1] + str[0] + str.slice(2, l - 3 + 1) + str[l - 1] + str[l - 2];
-  };
-  KornyMain = function() {
-    setTimeout(function() {
-      return $(".munged").each(function() {
-        var el;
-        el = $(this);
-        return el.html(demunge(el.attr("data-munged")));
-      });
-    });
-    return this;
-  };
-  $(function() {
-    return (window.KornyMain = {
-      App: KornyMain,
-      the_app: new KornyMain()
-    });
-  });
-}).call(this);
+function demunge(str) {
+  var l;
+  l = str.length;
+  return str[1] + str[0] + str.slice(2, l - 3 + 1) + str[l - 1] + str[l - 2];
+}
+
+document
+  .querySelectorAll(".munged")
+  .forEach((el) => (el.innerHTML = demunge(el.getAttribute("data-munged"))));
